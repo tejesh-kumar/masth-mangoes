e7e656128fd6e469c1946337351da6714ecf99b1
tejeshbm <tejesh@socialpilot.co>
2021-05-29 19:22:19 +0530

    setup sass files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Landing from './landing';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center text-success">Masth Mangoes</h1>
+      <Landing />
     </div>
   );
 }
---
+++ src/landing/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './landing.scss';
+
+function index(props) {
+    return (
+        <h2 className="text-center mango">
+            Landing Page
+        </h2>
+    )
+}
+
+index.propTypes = {
+
+}
+
+export default index
+
